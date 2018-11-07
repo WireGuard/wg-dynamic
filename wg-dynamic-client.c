5bbd32aaefa1baacdd8edd90efd4f8b609cbbc87
Tushar Pankaj <tushar.s.pankaj@gmail.com>
2018-11-06 23:12:56 -0600

    Write basic main
    Signed-off-by: Tushar Pankaj <tushar.s.pankaj@gmail.com>

--- wg-dynamic-client.c
+++ wg-dynamic-client.c
@@ -2,3 +2,32 @@
 /*
  * Copyright (C) 2018 Wireguard LLC
  */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+const char *PROG_NAME;
+
+/* TODO: break this function out into another file when it gets big */
+static void connect_to_server(char *interface)
+{
+}
+
+static void show_usage()
+{
+	fprintf(stderr, "Usage: %s <interface>\n\n", PROG_NAME);
+}
+
+int main(int argc, char *argv[])
+{
+	PROG_NAME = argv[0];
+
+	if (argc == 1) {
+		show_usage();
+		return EXIT_FAILURE;
+	}
+
+	connect_to_server(argv[1]);
+
+	return EXIT_SUCCESS;
+}
--- wg-dynamic-server.c
+++ wg-dynamic-server.c
@@ -2,3 +2,32 @@
 /*
  * Copyright (C) 2018 Wireguard LLC
  */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+const char *PROG_NAME;
+
+/* TODO: break this function out into another file when it gets big */
+static void setup_server(char *interface)
+{
+}
+
+static void show_usage()
+{
+	fprintf(stderr, "Usage: %s <interface>\n\n", PROG_NAME);
+}
+
+int main(int argc, char *argv[])
+{
+	PROG_NAME = argv[0];
+
+	if (argc == 1) {
+		show_usage();
+		return EXIT_FAILURE;
+	}
+
+	setup_server(argv[1]);
+
+	return EXIT_SUCCESS;
+}
