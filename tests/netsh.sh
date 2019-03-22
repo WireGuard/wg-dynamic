a20ea1cd4131ea7ccd49f9c861174dc3f2e5281b
Linus Nordberg <linus@nordberg.se>
2019-03-22 09:47:07 +0100

    print pid; optionally run server under gdb

--- tests/netsh.sh
+++ tests/netsh.sh
@@ -79,4 +79,9 @@ n2 wg set wg0 peer "$server_public" endpoint [::1]:1
 n2 ping6 -c 10 -f -W 1 fe80::%wg0
 n1 ping6 -c 10 -f -W 1 fe80::badc:0ffe:e0dd:f00d%wg0
 
-n1 ./wg-dynamic-server wg0
+pp echo "PID: $$"
+if [ -z "$NETSH_GDB" ]; then
+    n1 ./wg-dynamic-server wg0
+else			     # Try NETSH_GDB="-ex run" tests./netsh.sh
+    n1 gdb $NETSH_GDB --args ./wg-dynamic-server wg0
+fi
